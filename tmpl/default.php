2f52eebd1afeef2f09c75245a9831edba79574fe
Boris Barroso <boriscyber@gmail.com>
2009-08-09 15:30:43 -0400

    jQuery, modal, tabs

--- tmpl/default.php
+++ tmpl/default.php
@@ -8,12 +8,98 @@
   z-index: 100;
 }
 </style>
+<!--Tabs CSS -->
+    <style type="text/css" media="screen">
+    <!--
+		BODY { margin: 10px; padding: 0; font: 1em "Trebuchet MS", verdana, arial, sans-serif; font-size: 100%; }
+		H1 { margin-bottom: 2px; font-family: Garamond, "Times New Roman", Times, Serif;}
+		DIV.container { margin: auto; width: 90%; margin-bottom: 10px;}
+		TEXTAREA { width: 80%;}
+		FIELDSET { border: 1px solid #ccc; padding: 1em; margin: 0; }
+		LEGEND { color: #ccc; font-size: 120%; }
+		INPUT, TEXTAREA { font-family: Arial, verdana; font-size: 125%; padding: 7px; border: 1px solid #999; }
+		LABEL { display: block; margin-top: 10px; } 
+		IMG { margin: 5px; }
+
+		UL.tabNavigation {
+		    list-style: none;
+		    margin: 0;
+		    padding: 0;
+		}
+
+		UL.tabNavigation LI {
+		    display: inline;
+		}
+
+		UL.tabNavigation LI A {
+		    padding: 3px 5px;
+		    background-color: #ccc;
+		    color: #000;
+		    text-decoration: none;
+		}
+
+		UL.tabNavigation LI A.selected,
+		UL.tabNavigation LI A:hover {
+		    background-color: #333;
+		    color: #fff;
+		    padding-top: 7px;
+		}
+		
+		UL.tabNavigation LI A:focus {
+			outline: 0;
+		}
+
+		div.tabs > div {
+			padding: 5px;
+			margin-top: 3px;
+			border: 5px solid #333;
+		}
+		
+		div.tabs > div h2 {
+			margin-top: 0;
+		}
+
+		#first {
+		    background-color: #f00;
+		}
+
+		#second {
+		    background-color: #0f0;
+		}
+
+		#third {
+		    background-color: #00f;
+		}
+		
+		.waste {
+			min-height: 1000px;
+		}
+    -->
+    </style>
+
+<script src="<?php echo JURI::base() ?>modules/mod_timeline/tmpl/jquery-1.3.2.min.js" type="text/javascript"></script>
+<script type="text/javascript">
+// Tabs script
+$(function () {
+  var tabContainers = $('div.tabs > div');
+  tabContainers.hide().filter(':first').show();
+  
+  $('div.tabs ul.tabNavigation a').click(function () {
+    tabContainers.hide();
+    tabContainers.filter(this.hash).show();
+    $('div.tabs ul.tabNavigation a').removeClass('selected');
+    $(this).addClass('selected');
+    return false;
+  }).filter(':first').click();
+});
+</script>
+
+
 <script type="text/javascript">
 var timeline_data = <?php echo json_encode($json); ?>;
 //Timeline_urlPrefix = '<?php echo JURI::base(); ?>/modules/mod_timeline/tmpl/timeline_js/';
 Timeline_urlPrefix = "http://static.simile.mit.edu/timeline/api-2.3/";
 </script>
-<script src="<?php echo JURI::base() ?>modules/mod_timeline/tmpl/Sly-min.js" type="text/javascript"></script>
 
 <script type="text/javascript">
      Timeline_ajax_url="<?php echo JURI::base(); ?>modules/mod_timeline/tmpl/timeline_ajax/simile-ajax-api.js";
@@ -34,9 +120,9 @@ Timeline_urlPrefix = "http://static.simile.mit.edu/timeline/api-2.3/";
     //s = Sly.find("div.simileAjax-bubble-container");
     //s.remove();
     url = "<?php echo JURI::base(); ?>index.php?option=com_content&view=article&format=ajax&id=" + this.getID();
-    console.log(url);
-    //s.find("#");
-    console.log("%s, %s, %s",this.getID(), this.getText(), this.getDescription());//this.getID(), this.getText(), this.getDescription();
+    $.getJSON(url, function(data){
+      //
+    });
   }
   /*Timeline.DefaultEventSource.Event.prototype.fillInfoBubble = function(elmt, theme, labeller) {
     //elmt.id = "contTimeline";
