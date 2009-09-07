5aac11271bf979a8e09e1cd9f87e878759481dee
Boris Barroso <boriscyber@gmail.com>
2009-09-07 07:16:36 -0400

    Fix month width

--- mod_timeline.php
+++ mod_timeline.php
@@ -3,7 +3,7 @@ defined('_JEXEC') or die('Direct Access to this location is not allowed.');
 
 require_once(dirname(__FILE__).DS.'helper.php');
 $sectionid = $params->get('sectionid');
-$interval = 300;// $params->get('interval') ? $params->get('interval') : 200;
+$interval = $params->get('interval') ? $params->get('interval') : 200;
 
 $tm = new ModTimelineHelper($sectionid);
 $json = array();
